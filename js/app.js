531ce2627cc1103924bde66833a47485bd3859bd
loreV <lore.verri@gmail.com>
2015-05-24 15:49:35 +0200

    Start working on playback

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-// DOMContentLoaded is fired once the document has been loaded and parsed,
-// but without waiting for other external resources to load (css/images/etc)
-// That makes the app more responsive and perceived as faster.
-// https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
-
+/*!
+ *  Firebeat v1.0
+ *  www.themovingweb.com/firebeat/
+ *
+ *   2015, Lorenzo Verri
+ *
+ *  MIT License
+ */
 var F;
 
 F = {
@@ -60,49 +63,172 @@ F = {
             } catch (e){
                 console.warn("No firefox OS")
             }
+        },
+        prepareGrid: function(){
+
+
+        },
+
+        editor: {
+            /**
+             * Adds a new track
+             * @param trackNr
+             */
+            addTrack: function(trackNr){
+                $('#mu-list').append('<section class="mu-controller mu-controller-red" id="mu-'+trackNr+'"><span class="mu-title">Track '+trackNr+'</span><div class="option" id="mu-op-'+trackNr+'"><img src="css/player/icons/left_controls.png" class="mu-settings" /></div></section>');
+                $('.mu-controller').css('height', F.editor.row_size);
+                $('#mu-'+trackNr).click(function(e){F.ui.editor.showTrackSettingPanel(e);});
+
+
+                // If the setting panel is already showing than focus on that.
+                //if()
+            },
+            /**
+             * Remove an existing track
+             * @param trackNr
+             */
+            removeTrack: function(trackNr){
+
+            },
+
+            /**
+             * Show the track panel and applies UI changes
+             * TODO -> improve the DOM querying
+             * @param event object sent by the press of the button
+             */
+            showTrackSettingPanel : function(event){
+
+                $('.mu-controller').removeClass('blueBackground');
+                $(event.currentTarget).addClass('blueBackground');
+                event = event.currentTarget.id.split('-')[1];
+                // move from the left the control panel
+                // enough pixels to still have everything showing
+                $('#mu-settings-panel').removeClass('hiddenElement');
+                $('#mu-settings-panel').css('left', F.editor.col_size*2);
+                // hide non usable controls (play and so)
+                $('#right-side-controls').addClass('hiddenElement');
+            },
+            /**
+             *
+             */
+            hideTrackSettingPanel : function(){
+                $('.mu-controller').removeClass('mu-controller-red');
+                $('#mu-settings-panel').removeClass('visibleElement');
+                $('#right-side-controls').removeClass('hiddenElement');
+            },
+
+            /**
+             * Add a new tile only if the tile was not added yet. Else it removes it.
+             * @param {Event} e
+             * @param {boolean} isToAdd
+             */
+            addTile : function(eX, eY, isToAdd){
+                if(isToAdd){
+                    F.editor.co.fillStyle = '#FF3300';
+                } else {
+                    var color1 = "#fff3e0";
+                    var color2= "#f9fbe7";
+                    var colorToUse = "";
+                    if((eY % 2) == 0){
+                        if(eX % 2 == 0){
+                            colorToUse = color2;
+                        } else {
+                            colorToUse = color1;
+                        }
+                    } else {
+                        if(eX % 2 == 0){
+                            colorToUse = color1;
+                        } else {
+                            colorToUse = color2;
+                        }
+                    }
+                    F.editor.co.fillStyle = colorToUse;
+                }
+
+                F.editor.co.fillRect((eX * F.editor.col_size), (eY* F.editor.row_size) , F.editor.col_size , F.editor.row_size);
+            }
+
+
+        },
+
+
+
+
+        refreshUI: function(){
+            $('.mu-controller').css("height", F.editor.row_size);
+
+            //todo way more should actually go in this method!
+            var play_btn = document.getElementById("control-play");
+            play_btn.style.width = F.editor.col_size/1.1 +"px";
+            play_btn.style.height = F.editor.col_size/1.1 +"px";
+
+            var menu_btn = document.getElementById('control-menu');
+            menu_btn.style.width = F.editor.col_size/2 + "px";
+            menu_btn.style.height = F.editor.col_size/2 + "px";
+
+            var controllerBox = document.getElementById('right-side-controls');
+            controllerBox.style.width = (play_btn.width*3) + " px";
+            controllerBox.style.height = (controllerBox.style.width / 3);
+
+
         }
-
-
-
-
     },
 
     editor: {
 
         look:{
-            x:0,
-            y:0
+            x : 0,
+            y : 0
         },
+        row_size:0,
+        col_size:0,
+        row_number: 0,
+        col_number: 0,
 
         c: {},
         co: {},
-        grid: {},
+        sideControls:{},
+
+
 
 
 
         init: function () {
 
-            // TODO -> turn the screen & lock it
+            this.look.x = window.innerWidth;
+            this.look.y = window.innerHeight;
+
+            var container = document.getElementById("app-container");
+            container.width= this.look.x;
+            container.height = this.look.y;
+
+            this.sideControls = document.getElementById('side-controls');
+
             this.c = document.getElementById("editor-main");
-            this.c.width = window.innerWidth;
+            this.c.width = window.innerWidth - parseInt($('#side-controls').css('width').split('px')[0]);
             this.c.height = window.innerHeight;
             this.co = F.editor.c.getContext("2d");
+            this.newGrid();
 
-            //F.ui.changeOrientation();
+            var muLast = document.getElementById('mu-last');
+            muLast.addEventListener('click', function(){ F.editor.addTrack(); });
+            this.c.addEventListener('click', function(e){F.editor.select(e); });
+            //$('#editor-main').click(function(e){F.editor.select(e); });
+            $('#control-play').click(function(){ F.editor.controls.play(); });
 
-            console.log(screen.orientation);
-            F.editor.newGrid();
-
-            //screen.addEventListener("orientationchange",
-            //    function(){
-            //      },
-            //    false);
-
+            F.ui.refreshUI();
+            this.controls.init();
+            //this.addListener();
         },
 
+        /**
+         * Creates a new grid. This method should be called only on creation.
+         */
         newGrid: function () {
-            var x =window.innerWidth;
-            var y =window.innerHeight;
+            this.grid = [];
+
+            var x = this.c.width;
+            var y = this.look.y;
 
             var row_min_height = 60;
             var min_row_number = 4;
@@ -117,15 +243,18 @@ F = {
             }
 
 
-            var row_height = y /actualNrRows;
+            this.row_size = parseInt(y /actualNrRows);
 
             // then count how many columns we are going to have;
-            var min_num_columns = 12;
-            var column_size = x / 12;
-
+            var min_num_columns = 10;
+            console.log(x, min_num_columns);
+            this.col_size = parseInt(x / min_num_columns);
 
             // rows should be high enough to fill up the space
             //if(window.innerHeight > 320) nrRows = window.innerHeight  ;
+
+            this.row_number = actualNrRows;
+            this.col_number = min_num_columns;
 
             var color1 = "#fff3e0";
             var color2= "#f9fbe7";
@@ -134,29 +263,112 @@ F = {
             var diffBackground2 = true;
             for (var i = 0; i < actualNrRows; i++) {
                 F.editor.co.fillStyle = diffBackground ? color1 : color2;
-                F.editor.co.fillRect(0, (i * row_height), x , row_height);
+                F.editor.co.fillRect(0, (i * this.row_size), x , this.row_size);
 
                 for(var z = 0 ; z < min_num_columns; z++){
-                    // Render the controller
-                    // if z == 0;
-                    F.editor.co.fillStyle = diffBackground2 ? color2 : color1 ;
-                    F.editor.co.fillRect((z * column_size), (i*row_height) , column_size , row_height);
+                    this.co.fillStyle = diffBackground2 ? color2 : color1 ;
+                    this.co.fillRect((z * this.col_size), (i*this.row_size) , this.col_size , this.row_size);
                     diffBackground2 = !diffBackground2;
                 }
-
-
-
+                // controls
                 diffBackground = !diffBackground;
                 diffBackground2 = !diffBackground2;
             }
+        },
+        // Creates a new beat
+        newBeat:function(){
+
+        },
+        /**
+         * Checks a position in the gri to add a tile to it.
+         * @param {Event} e
+         */
+        select: function(e){
+            // Verify whether grid already has assigned a tile
+            // determine X and Y
+            var offsetX = parseInt($('#side-controls').css('width').split('px')[0]);
+            var x = e.clientX - offsetX;
+            var y = e.clientY;
+            //console.log(x, y);
+            var eX = parseInt(x / F.editor.col_size);
+            var eY = parseInt(y / F.editor.row_size);
+
+            if(this.controls.tracks.length > eY){
+                var isToADD = (this.controls.grid[eX][eY]==undefined) ? true : false;
+                F.ui.editor.addTile(eX,eY, isToADD);
+            }
+        },
+        addTrack: function(){
+            // if tracks are less than 10 -- for memory reasons
+            if(this.controls.tracks.length < 10){
+                F.ui.editor.addTrack( this.controls.tracks.length );
+                this.controls.tracks.push(
+                    new Track(
+                        new Howl({
+                            urls: ['data/effects/bass.wav'],
+                            buffer: true
+                        })
+                    )
+                );
+            }
+        },
+        removeTrack: function(index){
+
+        },
+        /**
+         * Contains
+         */
+        controls : {
+            stop : false,
+            cursor : 0,
+            grid: [],
+            tracks : [],
+            interval: 500,
+
+            init: function(){
+                for(var i = 0;  i < F.editor.col_number ; i++)
+                {
+                    this.grid[i] = new Array(F.editor.row_number);
+                }
+                console.log(this.grid);
+            },
+
+            play: function () {
+                if (this.tracks.length == 0) {
+                    alert('No tracks added');
+                } else {
+                    if(!this.stop){
+                        this.load();
+                    }
+                }
+            },
+            load: function(){
+
+                if(this.cursor < this.tracks.length){
+                    // load next sound
+                    //var obj = this.tracks[this.cursor].getAudioObj();
+                    this.cursor++;
+                    this.load();
+                } else {
+                    // play
+                    this.cursor = 0;
+                    this.playSound();
+                }
+            },
+            playSound: function () {
+                // Play right sound
+                setTimeout(this.playSound, this.interval);
+            }
+        },
+        /**
+         * Settings
+         */
+        trackSettings : {
 
 
         },
 
 
-        newBeat: function () {
-
-        },
 
         /***
          * Clean objects around and quit the editor
--- js/classes/Track.js
+++ js/classes/Track.js
@@ -1,3 +1,35 @@
 /**
  * Created by lore on 23.05.15.
  */
+function Track(defaultAudio){
+    this.name = "";
+    this.path = "";
+    this.type = "audio"; // Should be either audio or recording
+    this.audioObj = defaultAudio;
+}
+
+Track.prototype.setName = function(name){
+    this.name = name;
+};
+
+Track.prototype.getName = function(){
+    return this.name;
+};
+
+Track.prototype.setType = function(type){
+    if(type != "audio" && type != "recording"){
+        throw new Error("Track should be either 'audio' or 'recording'");
+    } else {
+        this.type = type;
+    }
+};
+
+Track.prototype.getType = function(){
+    return this.type;
+};
+
+Track.prototype.getAudioObj = function () {
+    return this.audioObj;
+};
+
+
